9df3c6d84121f45c9260c2c8125cc6d37dbf0253
phil <philipmoran83@outlook.com>
2024-01-12 20:33:33 +0000

    completed exercise

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,35 @@
+package com.serenitydojo;
+
+public class Dog {
+    private String name;
+    private String favouriteToy;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavouriteToy() {
+        return favouriteToy;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setFavouriteToy(String favouriteToy) {
+        this.favouriteToy = favouriteToy;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Dog(String name, String favouriteToy, int age) {
+        this.name = name;
+        this.favouriteToy = favouriteToy;
+        this.age = age;
+    }
+}
+
